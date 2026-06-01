f1e44b345bad4a60398cafe4c33cb697bc27ecf7
hkforgame7862-lang <hkforgame7862@gmail.com>
2026-06-01 04:16:23 -0700

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const mineflayer = require('mineflayer');
 
 function createBot() {
   const bot = mineflayer.createBot({
-    host: 'bananasmp.net',
+    host: 'play.bananasmp.net',
     port: 25565,
     username: 'Kythox_',
     auth: 'offline',
@@ -14,21 +14,16 @@ function createBot() {
 
   bot.on('spawn', () => {
     console.log('Spawned!');
-
-    // Auto login
     setTimeout(() => {
       bot.chat('/login hassan123pass0');
-      console.log('Logged in!');
+      console.log('Sent login!');
     }, 2000);
-
-    // Join randomkits
     setTimeout(() => {
       bot.chat('/server randomkits');
       console.log('Joined RandomKits!');
     }, 5000);
   });
 
-  // Anti-AFK
   setInterval(() => {
     bot.setControlState('sneak', true);
     setTimeout(() => bot.setControlState('sneak', false), 1000);
@@ -36,13 +31,13 @@ function createBot() {
 
   bot.on('kicked', (reason) => {
     console.log('Kicked:', reason);
-    setTimeout(createBot, 30000);
+    setTimeout(createBot, 300000); // 5 minutes
   });
   bot.on('error', (err) => {
     console.log('Error:', err.message);
-    setTimeout(createBot, 30000);
+    setTimeout(createBot, 300000); // 5 minutes
   });
-  bot.on('end', () => setTimeout(createBot, 30000));
+  bot.on('end', () => setTimeout(createBot, 300000)); // 5 minutes
 }
 
 createBot();
